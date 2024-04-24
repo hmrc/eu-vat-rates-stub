b1a9ab4d41a5e329903ae41044c1c72c14b314ac
Susan Badel <susan.badel@opencastsoftware.com>
2024-04-24 15:04:43 +0100

    VEIOSS-581 further updates

--- app/uk/gov/hmrc/euvatratesstub/controllers/VatRateController.scala
+++ app/uk/gov/hmrc/euvatratesstub/controllers/VatRateController.scala
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2024 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package uk.gov.hmrc.euvatratesstub.controllers
 
 import uk.gov.hmrc.play.bootstrap.backend.controller.BackendController
@@ -14,7 +30,7 @@ class VatRateController @Inject()(cc: ControllerComponents)
 
   def getEUVatRates: Action[AnyContent] = Action.async { implicit request =>
 
-    val responseXml = Try(Source.fromInputStream(getClass.getResourceAsStream("/resources/examples/example-request.xml")).mkString)
+    val responseXml = Try(Source.fromInputStream(getClass.getResourceAsStream("/resources/xml/response.xml")).mkString)
 
     Future.successful(Ok(responseXml.get))
   }
